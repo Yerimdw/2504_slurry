37c3c602763cc991e252e49a4a24ad9b95404ed5
Yerimdw <yerimdw@ajou.ac.kr>
2025-04-01 00:58:37 +0900

    Add files via upload

---
+++ st.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Random Forest", #크롬 페이지 이름 변경
+    page_icon="C:/Users/ludia/PycharmProjects/250327_randomforest_st/image/battery.png"
+)
+
+st.title("Hello streamlit")
